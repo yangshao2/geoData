083e52c245070166b17b5b662ec8642940dbf79c
yangshao2 <yshao@vt.edu>
2024-07-08 20:45:31 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' CBG Data
+#'
+#' This dataset contains CBG data.
+#'
+#' @format An sf object.
+#' @usage data(cbg)
+#' @examples
+#' data(cbg)
+#' plot(cbg)
+"cbg"
+
+#' City Data
+#'
+#' This dataset contains City data.
+#'
+#' @format An sf object.
+#' @usage data(city)
+#' @examples
+#' data(city)
+#' plot(city)
+"city"
+
